105081dd848818dbc10d10fcb5bfbd565ef13ccb
andreikurnosov <nixdie@yandex.ru>
2021-03-17 02:56:09 +0700

    sign up with react and firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,18 +1,34 @@
 import app from 'firebase/app'
+import 'firebase/auth'
 
 const config = {
-  apiKey: import.meta.env.REACT_APP_API_KEY,
-  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: import.meta.env.REACT_APP_DATABASE_URL,
-  projectId: import.meta.env.REACT_APP_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
 }
 
 class Firebase {
   constructor() {
     app.initializeApp(config)
+
+    this.auth = app.auth()
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password)
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password)
+
+  doSignOut = () => this.auth.signOut()
+
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email)
+
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password)
 }
 
 export default Firebase
